4aa999330bd38d4d29692516b86ab897cd5a9cc3
bmcho <bmcho91@naver.com>
2022-07-13 17:04:12 +0900

    add: top-ranking-list 로직 추가 (#7)
    mysql 8.0으로 업데이트 하면서 rank  사용가능해짐으로인하여 rank를 이요하여 로직 추가
    TODO: 내 랭킹을 불러오긴 위해서 subquery를 사용하려고하는데 django orm의 subquery사용이 명확하지가 않음

--- raid/serializers/raid_serializer.py
+++ raid/serializers/raid_serializer.py
@@ -3,14 +3,11 @@ from rest_framework import serializers
 
 
 class BossRaidRakingSerializer(serializers.Serializer):
-    user_id = serializers.CharField()
+    rank = serializers.IntegerField()
+    nickname = serializers.CharField()
     total_score = serializers.IntegerField()
 
 
-class BossRaidHistorySerializer(serializers.ModelSerializer):
+class BossRaidHistorySerializer(serializers.Serializer):
     top_ranker_info_list = BossRaidRakingSerializer(many=True)
     my_ranking = BossRaidRakingSerializer()
-
-    class Meta:
-        model = models.RaidHistory
-        fields = '__all__'
