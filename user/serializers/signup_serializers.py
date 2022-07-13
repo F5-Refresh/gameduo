cd835e29e2947be55ac141f7d7d234d9650cb067
heyhyo11 <1326cy@gmail.com>
2022-07-13 11:44:29 +0900

    회원가입 시리얼라이저에서 남은 병합 기록 수정 (#4)

--- user/serializers/signup_serializers.py
+++ user/serializers/signup_serializers.py
@@ -6,21 +6,13 @@ from user.models import User
 
 # 회원가입 serializer 입니다.
 class SignUpSerializer(serializers.ModelSerializer):
-<<<<<<< HEAD
     """회원가입 Serializer
 
     Writer: 전기원
     Date: 2022-07-12
 
     """
-    def create(self, validated_data):
-        password = validated_data.get('password')
-        
-        # 비밀번호 8~15자/소문자,숫자 최소하나 사용
-        # 정규식 객체를 리턴 
-        password_re =re.compile('^(?=.{8,15}$)(?=.*[a-z])(?=.*[0-9]).*$')
-        
-=======
+
     def create(self, validated_data):
         password = validated_data.get('password')
 
@@ -28,7 +20,6 @@ class SignUpSerializer(serializers.ModelSerializer):
         # 정규식 객체를 리턴
         password_re = re.compile('^(?=.{8,15}$)(?=.*[a-z])(?=.*[0-9]).*$')
 
->>>>>>> 59780ff (로그인, 로그아웃, JWT 발급 기능구현 (#4))
         if not re.match(password_re, password):
             raise serializers.ValidationError({"password": ["비밀번호는 8~15자의 영소문자와 숫자로 이루어져야합니다."]})
 
