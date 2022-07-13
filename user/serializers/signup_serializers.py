0a1a6fa6e80a2568ffcd07c1f8ddc93da8dad446
KIWONY <kien1004@naver.com>
2022-07-13 11:10:12 +0900

    modify:주석 수정(#5)

--- user/serializers/signup_serializers.py
+++ user/serializers/signup_serializers.py
@@ -6,12 +6,17 @@ from user.models import User
 
 # 회원가입 serializer 입니다.
 class SignUpSerializer(serializers.ModelSerializer):
+    """회원가입 Serializer
 
+    Writer: 전기원
+    Date: 2022-07-12
+
+    """
     def create(self, validated_data):
         password = validated_data.get('password')
         
-    # 비밀번호 8~15자/소문자,숫자 최소하나 사용
-    # 정규식 객체를 리턴 
+        # 비밀번호 8~15자/소문자,숫자 최소하나 사용
+        # 정규식 객체를 리턴 
         password_re =re.compile('^(?=.{8,15}$)(?=.*[a-z])(?=.*[0-9]).*$')
         
         if not re.match(password_re, password):
