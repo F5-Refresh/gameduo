2214a1334ea515d63c16f7eff8f5e3939f7e8635
heyhyo11 <1326cy@gmail.com>
2022-07-13 09:26:12 +0900

    로그인 시리얼라이저 추가 (#4)

--- gameduo/urls.py
+++ gameduo/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -14,11 +15,22 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+<<<<<<< HEAD
     path('users', include('user.urls')),
+=======
+    path('admin/', admin.site.urls),
+    path('', include('dj_rest_auth.urls')),
+    # path('accounts/', include('dj_rest_auth.registration.urls')),
+    # path('users', include('user.urls')),
+>>>>>>> 38910fb (로그인 시리얼라이저 추가 (#4))
 ]
 
 urlpatterns += [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$',schema_view.without_ui(cache_timeout=0),name='schema-json',),
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json',
+    ),
     re_path(r'^swagger$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,12 @@
 from django.urls import include, path
 
+<<<<<<< HEAD
 from user.views.signup_view import SignUpView
 
 urlpatterns = [
     path('/signup',SignUpView.as_view())
+=======
+urlpatterns = [
+    #  path('accounts/', include('dj_rest_auth.registration.urls')),
+>>>>>>> 38910fb (로그인 시리얼라이저 추가 (#4))
 ]
