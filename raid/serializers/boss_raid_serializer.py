36d38bcb8f5efe7de939f6e89bc051c870e09d14
pekopekopekopayo <dlehddus1285@gmail.com>
2022-07-14 22:15:26 +0900

    feat: 보스레이드시작/종료 기능작성 (#8)

--- raid/serializers/boss_raid_serializer.py
+++ raid/serializers/boss_raid_serializer.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.shortcuts import get_object_or_404
 from raid.models import RaidHistory
 from rest_framework import serializers
@@ -7,6 +8,11 @@ class BossRaidHistorySerializer(serializers.ModelSerializer):
     def create(self, data):
         user, level = data.get('user'), data.get('level')
         return RaidHistory.objects.create(user=user, level=level)
+
+    def validate_level(self, level):
+        if cache.get(f"level{level}") == None:
+            raise serializers.ValidationError({'detail': '유효한 레벨이 아닙니다.'})
+        return level
 
     class Meta:
         model = RaidHistory
