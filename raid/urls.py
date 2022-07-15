871208a98579812fb36f6ef2999c80ace9c2c045
pekopekopekopayo <dlehddus1285@gmail.com>
2022-07-15 10:05:47 +0900

    modify: 충돌으로 인한 urls.py파일 수정 (#8)

--- raid/urls.py
+++ raid/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
 from raid.views.boss_raid_view import BossRaidView
+from raid.views.raid_ranking import BossRaidRankingView
 from raid.views.raid_status_search import RaidStatusSearchView
 
-from .views import raid_ranking
-
 urlpatterns = [
-    path('/ranking', raid_ranking.BossRaidRankingView.as_view()),
     path('/status-search', RaidStatusSearchView.as_view()),
-    path('start_raid', BossRaidView.start_boss_raid),
-    path('end_raid', BossRaidView.end_boss_raid),
+    path('/ranking', BossRaidRankingView.as_view()),
+    path('/start_raid', BossRaidView.start_boss_raid),
+    path('/end_raid', BossRaidView.end_boss_raid),
 ]
